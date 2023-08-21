89abd570d8929e924ae75c6fe4310d3c3a2ea704
mfranke84 <markus.franke84@gmail.com>
2023-08-21 13:34:14 +0200

    Added more input fields

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ function handleEditProfile(){
 }
 
 function handleUpdateProfile(){
+    // update Name 
     var updateName = document.getElementById("inputName").value;
     var name = document.getElementById("name");
     name.textContent = updateName;
+
+    // Update email
+    var updateEmail = document.getElementById("inputEmail").value;
+    var email = document.getElementById("email");
+
+    email.textContent = updateEmail;
+    
+    // Update interests
+    var updateInterests = document.getElementById("inputInterests").value;
+    var interests = document.getElementById("interests");
+
+    interests.textContent = updateInterests;
     
     document.getElementById("display").style.display = "block";
     document.getElementById("edit-view").style.display = "none"; 
