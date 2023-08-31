a21a15d9ad64712e99d7cf82151847e175de9b5d
mfranke84 <markus.franke84@gmail.com>
2023-08-31 08:43:15 +0200

    Adjusted code to VUE.JS and added packages

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="display">
+    <h1>User Profile</h1>
+        <img src="profile.jpeg" alt="Profile Picture"> 
+        <span>Name: </span><b id="name"></b>
+        <hr>
+        <span>Email: </span><b id="email"></b>
+        <hr>
+        <span>Interests: </span><b id="interests"></b>
+        <hr>
+        <button onclick="handleEditProfile()">Edit profile</button>
+    </div>
+        <div id="edit-view">
+            <h1>User Profile</h1>
+            <img src="profile.jpeg" alt="Profile Picture"> 
+            <span>Name: </span><input id="inputName" type="text" placeholder="Jan Mustermann">
+            <hr>
+            <span>Email: </span><input id="inputEmail" type="text" placeholder="J.mustermann@example.com">
+            <hr>
+            <span>Interests: </span><input id="inputInterests" type="text" placeholder="Example Coding">
+            <hr>
+            <button onclick="handleUpdateProfile()">Update profile</button>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
