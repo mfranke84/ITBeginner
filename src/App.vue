91d061e9bc21e8f099aad33a2b507408ed096dc2
mfranke84 <markus.franke84@gmail.com>
2023-09-15 09:09:19 +0200

    Completed vue.js app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <template>
-<div id="display">
-    <h1>User Profile</h1>
-        <img src="profile.jpeg" alt="Profile Picture"> 
-        <span>Name: </span><b id="name"></b>
+    <div v-show="!isEditMode">
+        <h1>User Profile</h1>
+        <img :src="image" alt="Profile Picture"> 
+        <span>Name: </span><b id="name">{{ name }}</b>
         <hr>
-        <span>Email: </span><b id="email"></b>
+        <span>Email: </span><b id="email">{{ email }}</b>
         <hr>
-        <span>Interests: </span><b id="interests"></b>
+        <span>Interests: </span><b id="interests">{{ interests }}</b>
         <hr>
-        <button onclick="handleEditProfile()">Edit profile</button>
+        <button @click="handleEditProfile()">Edit profile</button>
+</div>
+    <div v-show="isEditMode">
+        <h1>User Profile</h1>
+        <img :src="image" alt="Profile Picture"> 
+        <span>Name: </span><input id="inputName" type="text" v-model="name">
+        <hr>
+        <span>Email: </span><input id="inputEmail" type="text" v-model="email">
+        <hr>
+        <span>Interests: </span><input id="inputInterests" type="text" v-model="interests">
+        <hr>
+        <button @click="handleUpdateProfile()">Update profile</button>
     </div>
-        <div id="edit-view">
-            <h1>User Profile</h1>
-            <img src="profile.jpeg" alt="Profile Picture"> 
-            <span>Name: </span><input id="inputName" type="text" placeholder="Jan Mustermann">
-            <hr>
-            <span>Email: </span><input id="inputEmail" type="text" placeholder="J.mustermann@example.com">
-            <hr>
-            <span>Interests: </span><input id="inputInterests" type="text" placeholder="Example Coding">
-            <hr>
-            <button onclick="handleUpdateProfile()">Update profile</button>
-        </div>
 </template>
 
 <script>
+    import image from "./profile.jpeg"
     export default {
-        name: "App"
+        name: "App",
+        data() {
+            return {
+                image: image,
+                name: "Anna Smith",
+                email: "anna.smith@gmail.com",
+                interests: "Coding",
+                isEditMode: true
+            }
+        },
+        methods: {
+            handleEditProfile(){
+                this.isEditMode = true;
+            },
+            handleUpdateProfile(){
+                this.isEditMode = false;
+            }
+        }
     }
+    
 </script>
+
+<style>
+img {
+    height: 270px;
+    display: block;
+    margin-bottom: 40px;
+    width: 320px;
+}
+
+div {
+    margin: 40px auto;
+    width: 80%;
+}
+
+hr {
+    width: 340px;
+    margin: 25px 0;
+}
+
+button {
+    width: 160px;
+    height: 45px;
+    font-size: 15px;
+    border-radius: 5px;
+}
+
+input {
+    height: 28px;
+    width: 200px;
+    padding: 5px;
+}
+</style>
