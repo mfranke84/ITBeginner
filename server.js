d56066543e859e742d724710683adaa1bc959d85
mfranke84 <markus.franke84@gmail.com>
2023-09-22 15:11:22 +0200

    Build server application

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-
-// Start app to listen on port 3000
-app.listen(3000, function(){
-    console.log("app listening on port 3000")
-});
+const bodyParser = require('body-parser')
 
 // Middleware
 app.use(bodyParser.json())
 
 // Provide index.html on root
-app.use('/', express.static(__dirname + '/'))
+app.use('/', express.static(__dirname + '/dist'))
+
+// Start app to listen on port 3000
+app.listen(3000, function(){
+    console.log("app listening on port 3000")
+});
