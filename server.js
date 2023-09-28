e637fed1f4ca44ac73acecee910bf444d6920678
mfranke84 <markus.franke84@gmail.com>
2023-09-28 12:17:35 +0200

    get data from db

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const app = express()
 const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb')
 
-var profile = {
-    name: "Anna Smith",
-    email: "anna.smith@gmail.com",
-    interests: "Coding"
-}
-
 // Connection URL
 const url = 'mongodb://localhost:27017'
 const client = new MongoClient(url)
@@ -24,22 +18,9 @@ app.use(bodyParser.json())
 // Provide index.html on root
 app.use('/', express.static(__dirname + '/dist'))
 
-app.get('/get-profile', function (req, res){
+app.get('/get-profile', async function (req, res){
     console.log("get-profile requested")
     
-    // connect to database
-
-    // get data from database
-    
-    
-    
-    res.send(profile)
-})
-
-app.post('/update-profile', async function(req, res){
-    const body = req.body
-    console.log(body)
-
     // connect to database
     await client.connect();
     console.log('Connected successfully to server');
@@ -48,22 +29,42 @@ app.post('/update-profile', async function(req, res){
     const db = client.db(dbName);
     const collection = db.collection(collName);
 
-    //save payload data to the database
-    const insertResult = await collection.insertMany([body]);
-    console.log('Inserted documents =>', insertResult);
-  
+    // get data from database
+    const result = await collection.findOne({ id: 1 });
+    console.log(result)
+    
+    
+    res.send(result)
+})
 
-    if(Object.keys(body).length === 0){
+app.post('/update-profile', async function(req, res){
+    const payload = req.body
+    console.log(payload)
+
+    //save payload data to the database
+    if(Object.keys(payload).length === 0){
         res.send({error: "Error: Payload is empty - Could not update user profile data"})
     }
     else{
+        // connect to database
+        await client.connect();
+        console.log('Connected successfully to server');
+
+        // initiate database
+        const db = client.db(dbName);
+        const collection = db.collection(collName);
+
         // Update profile
-        profile.name = body.name
-        profile.email = body.email
-        profile.interests = body.interests
+        payload['id'] = 1
+        const updatedPayload = { $set: payload }
+        const insertResult = await collection.updateOne({id: 1},updatedPayload,{upsert: true})
+        client.close()
+        console.log('Inserted documents =>', insertResult)
         // Send successfull message 
         res.status(200).send({info: "Profile updated successfully"})
     }
+
+
     return 'done.'
 
 
