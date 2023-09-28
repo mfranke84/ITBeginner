30c929b0902b7cf43e01a3ed20a56fb306b6c8d0
mfranke84 <markus.franke84@gmail.com>
2023-09-28 10:57:28 +0200

    added mongoDB and post operation

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
+
 var profile = {
     name: "Anna Smith",
     email: "anna.smith@gmail.com",
     interests: "Coding"
 }
+
+// Connection URL
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+// Database Name
+const dbName = 'company_db'
+const collName ='employees'
+
 
 // Middleware
 app.use(bodyParser.json())
@@ -15,11 +26,33 @@ app.use('/', express.static(__dirname + '/dist'))
 
 app.get('/get-profile', function (req, res){
     console.log("get-profile requested")
+    
+    // connect to database
+
+    // get data from database
+    
+    
+    
     res.send(profile)
 })
 
-app.post('/update-profile', function(req, res){
+app.post('/update-profile', async function(req, res){
     const body = req.body
+    console.log(body)
+
+    // connect to database
+    await client.connect();
+    console.log('Connected successfully to server');
+
+    // initiate database
+    const db = client.db(dbName);
+    const collection = db.collection(collName);
+
+    //save payload data to the database
+    const insertResult = await collection.insertMany([body]);
+    console.log('Inserted documents =>', insertResult);
+  
+
     if(Object.keys(body).length === 0){
         res.send({error: "Error: Payload is empty - Could not update user profile data"})
     }
@@ -31,7 +64,7 @@ app.post('/update-profile', function(req, res){
         // Send successfull message 
         res.status(200).send({info: "Profile updated successfully"})
     }
-
+    return 'done.'
 
 
     
